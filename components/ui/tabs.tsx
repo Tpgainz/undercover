8d14265abc537708efc86077e5c6af3bea9a63fb
Thomas ARPIN <tpain45200@gmail.com>
2024-01-17 01:15:08 +0100

    last commit

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -44,7 +44,7 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      "mt-2 ring-offset-background py-10 focus-visible:outline-none px-2 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      "mt-2 ring-offset-background  focus-visible:outline-none px-2 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
       className
     )}
     {...props}
